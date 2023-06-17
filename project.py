34f7f678261da39ca7054b3a0f635c8ad0fd13f2
silpaja <silpajachandrasekar@gmail.com>
2023-06-18 00:18:37 +0530

    Update project.py

--- project.py
+++ project.py
@@ -2,8 +2,8 @@ import requests
 import openai
 import re
 
-openai.api_key = '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'
-token = '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'
+openai.api_key = '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'
+token = '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'
 
 # GitHub API headers
 headers = {
@@ -20,6 +20,37 @@ def get_user_repositories(user):
     else:
         print(f"Failed to fetch repositories. Status code: {response.status_code}")
         return None
+
+def justify_complex_repository(complexity_evaluation):
+    # Implement your refined justification logic here based on specific keywords or phrases
+    justification = ""
+
+    # Check for specific keywords indicating complexity
+
+    # Keyword: "algorithm"
+    if 'algorithm' in complexity_evaluation.lower():
+        justification += "The code in this repository implements complex algorithms, showcasing advanced problem-solving techniques."
+
+    # Keyword: "optimization"
+    if 'optimization' in complexity_evaluation.lower():
+        justification += "The repository includes optimization techniques that enhance performance and efficiency."
+
+    # Keyword: "concurrency"
+    if 'concurrency' in complexity_evaluation.lower():
+        justification += "The code demonstrates handling concurrent processes or threads, indicating the complexity of managing parallel execution."
+
+    # Keyword: "machine learning"
+    if 'machine learning' in complexity_evaluation.lower():
+        justification += "The repository incorporates machine learning algorithms, indicating advanced data analysis and predictive modeling capabilities."
+
+    # Add more specific keywords or phrases as per your requirements
+
+    # If no specific aspects were found, provide a generic justification
+    if not justification:
+        justification = "The complexity evaluation suggests that the repository contains technically complex code, indicating a higher level of sophistication and expertise in the development process."
+
+    return justification
+
 
 # Preprocess code from repository
 def preprocess_code(code):
@@ -47,8 +78,10 @@ def generate_complexity_evaluation(prompt):
         prompt=prompt[:4000],  # Limit the prompt length to 4000 tokens
         max_tokens=100
     )
-
+    
     return response.choices[0].text.strip()
+    justification = response.choices[0].text.strip()
+
 
 # Calculate complexity score
 def calculate_complexity_score(complexity_evaluation):
@@ -93,10 +126,16 @@ def main():
                 prompt = f"Evaluate the technical complexity of the code in the repository '{repo_name}':\n{preprocessed_code}\n"
                 complexity_evaluation = generate_complexity_evaluation(prompt)
 
+                justification = justify_complex_repository(complexity_evaluation)
+                print("Justification for selecting the complex repository:")
+                print(justification)
+
                 # Calculate complexity score
                 complexity_score = calculate_complexity_score(complexity_evaluation)
                 complexity_scores.append((repo_name, complexity_score))
                 print(f"Complexity Score: {complexity_score}\n")
+
+                print(justification)
 
         # Sort repositories by complexity score
         complexity_scores.sort(key=lambda x: x[1], reverse=True)
